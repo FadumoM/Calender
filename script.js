aa4e191f5daae6a76bf33342eab88641b23be47e
FadumoM <fadumomohamed011@gmail.com>
2022-01-13 11:56:05 +0300

    small changes

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-var akan = function(y, m, d, g) {
-  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  var d = new Date(y, --m, d);
-  if (g === "Female") {
-      return d && femaleNames[d.getDay()];
-  } else {
-      return d && maleNames[d.getDay()];
-  }
-}
 
 
 
